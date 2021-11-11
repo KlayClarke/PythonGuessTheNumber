cf310958f25660d6808950b668dc7bf826fe2935
Klay Clarke <klayaclarke@gmail.com>
2021-11-11 01:10:54 -0500

    add logo

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,11 +1,20 @@
 import random
 
+logo = """ 
+.d88b                          88888 8           w     8b  8                 8               
+8P www 8   8 .d88b d88b d88b     8   8d8b. .d88 w8ww   8Ybm8 8   8 8d8b.d8b. 88b. .d88b 8d8b 
+8b  d8 8b d8 8.dP' `Yb. `Yb.     8   8P Y8 8  8  8     8  "8 8b d8 8P Y8P Y8 8  8 8.dP' 8P   
+`Y88P' `Y8P8 `Y88P Y88P Y88P     8   8   8 `Y88  Y8P   8   8 `Y8P8 8   8   8 88P' `Y88P 8    
+                                                                                             
+ """
+
 possible_numbers = []
 
 for num in range (1, 101):
     possible_numbers.append(num)
 
 def play():
+    print(logo)
     end_of_game = False
     random_number = random.choice(possible_numbers)
 
